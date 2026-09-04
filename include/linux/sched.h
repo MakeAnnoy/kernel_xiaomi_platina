a713074c5196d0f33f8b285a28b159517563f3d8
andrey167 <andrey_167@bk.ru>
2026-09-04 18:07:26 +0400

    sched: Add sched_set_boost stub and tracepoint
    Add inline stub for sched_set_boost() in sched.h returning -EINVAL
    and define corresponding TRACE_EVENT(sched_set_boost) for scheduler
    boost event tracing.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2666,6 +2666,11 @@ union thread_union {
 	unsigned long stack[THREAD_SIZE/sizeof(long)];
 };
 
+static inline int sched_set_boost(int enable)
+{
+	return -EINVAL;
+}
+
 #ifndef __HAVE_ARCH_KSTACK_END
 static inline int kstack_end(void *addr)
 {
