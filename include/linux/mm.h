15fd35524716ef4f052e9e99d523719739a88281
Lolipop <makeanonim@gmail.com>
2026-09-07 14:43:47 +0000

    Revert "prlmk: Rewrite to improve clarity"
    This reverts commit e36b4c20dc74a01b1779df1bb4c790d4f682aca0.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2404,7 +2404,7 @@ void __init setup_nr_node_ids(void);
 static inline void setup_nr_node_ids(void) {}
 #endif
 
-#if defined(CONFIG_PROCESS_RECLAIM) || defined(CONFIG_PRLMK)
+#ifdef CONFIG_PROCESS_RECLAIM
 struct reclaim_param {
 	struct vm_area_struct *vma;
 	/* Number of pages scanned */
